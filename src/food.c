3cd52e754e72bde7216682ed57fbe290d8c659a4
Leslye Jeannin <leslye1.jeannin@epitech.eu>
2026-06-19 15:14:39 +0400

    FEAT - random food spawn on a free cell

--- src/food.c
+++ src/food.c
@@ -1,2 +1,14 @@
 #include "food.h"
 #include <stdlib.h>
+
+void food_spawn(Food *f, const Snake *s, int grid_w, int grid_h)
+{
+    int x, y;
+    do {
+        x = rand() % grid_w;
+        y = rand() % grid_h;
+    } while (snake_occupies(s, x, y));
+
+    f->x = x;
+    f->y = y;
+}
